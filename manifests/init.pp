36be3653ffa9e91406b45fdbdaafd98963c25d7a
Nick Walker <nick.walker@puppetlabs.com>
2016-03-30 09:42:58 -0700

    Chnage module from stash to bitbucket server
    Prior to this commit, this profile was for setting up stash using
    puppet/stash but now atlassian changed the name of stash to
    bitbucket server and changed the installer a bit.
    
    After this commit, this profile using thewired/bitbucket module
    and made some changes to the profile to meet the dependency
    requirements for bitbucket server.  These include java 8 and
    git 1.8+

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,29 +1,40 @@
-# stash profile based on puppet/stash
-class rampup_profile_stash {
+# bitbucket profile based on thewired/bitbucket
+class rampup_profile_bitbucket_server {
+
+  #The bitbucket module uses archive or staging module
+  #which requires unzip but doesn't seem to install it
+  package { 'unzip':
+    ensure => present,
+  }
+
+  package { 'git' :
+    ensure => present,
+  }
 
   class { 'java' :
-    version => present,
+    package => 'java-1.8.0-openjdk-devel',
   } ->
 
   class { 'postgresql::globals':
     manage_package_repo => true,
     version             => '9.4',
-  }->
+  } ->
   class { 'postgresql::server': } ->
 
-  postgresql::server::db { 'stash':
-    user     => 'stash',
-    password => postgresql_password('stash', 'password'),
+  #class { 'bitbucket::gc': }
+  #class { 'bitbucket::facts': }
+
+  postgresql::server::db { 'bitbucket':
+    user     => 'bitbucket',
+    password => postgresql_password('bitbucket', 'password'),
   } ->
 
-  class { 'stash':
-    javahome  => '/etc/alternatives/java_sdk',
+  class { 'bitbucket':
+    javahome    => '/etc/alternatives/java_sdk',
     #dev.mode grants a 24-hour license for testing
-    java_opts => '-Datlassian.dev.mode=true',
-  }
-
-  file { '/opt/puppetlabs/bin/stash_mco.rb':
-    source => 'puppet:///modules/r10k/stash_mco.rb',
+    java_opts   => '-Datlassian.dev.mode=true',
+    version     => '4.4.1',
+    require     => [ Package['git', 'unzip'] ]
   }
 
 }
